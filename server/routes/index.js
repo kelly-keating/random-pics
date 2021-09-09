24f25794712c082a2ab10a0df1a29da5f66a91f8
Kelly Keating <keating.kel@gmail.com>
2021-09-10 10:54:45 +1200

    lol - add res.json

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,11 +8,12 @@ router.get('/', (req, res) => {
   const pics = {}
   const housesPath = path.join(__dirname, '..', 'public', 'houses')
   const randomPath = path.join(__dirname, '..', 'public', 'random')
-  
+
   fs.promises.readdir(housesPath)
     .then(houses => pics.houses = houses)
     .then(() => fs.promises.readdir(randomPath))
     .then(other => pics.other = other)
+    .then(() => res.json(pics))
     .catch(err => res.status(500).send('oh no! ' + err.message))
 })
 
